7b4bc8a98bdfc1a6b711456d330d297e61c5e3a1
Dmitry Didenko <ddidenko@smedialink.com>
2019-04-09 15:16:16 +0300

    qwe1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     // Весь код регистрации у нас асинхронный.
-    navigator.serviceWorker.register('/sw-testing/sw.js')
+    navigator.serviceWorker.register('/sw-testing/sw.js',{scope:'./cached'})
       .then(() => navigator.serviceWorker.ready.then((worker) => {
         worker.sync.register('syncdata');
       }))
