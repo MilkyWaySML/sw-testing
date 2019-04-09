719045aa51ece84aaff6c5390cdfd633177970cd
Dmitry Didenko <ddidenko@smedialink.com>
2019-04-09 15:28:12 +0300

    qwe123123

--- sw.js
+++ sw.js
@@ -4,8 +4,7 @@ self.addEventListener('install', (event) => {
 
     event.waitUntil(caches.open(CACHE).then(function (cache) {
     cache.addAll([
-      './cached',
-      './index.html'
+      './cached'
     ]);
   }));
 });
