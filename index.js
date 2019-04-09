51be1f47166f888cd412fff6b5052467e4692942
Dmitry Didenko <ddidenko@smedialink.com>
2019-04-09 13:59:37 +0300

    big update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     // Весь код регистрации у нас асинхронный.
-    navigator.serviceWorker.register('/sw-testing/sw.js',{ scope: '/sw-testing/static/'})
+    navigator.serviceWorker.register('/sw-testing/sw.js',{ scope: '/sw-testing/'})
       .then(() => navigator.serviceWorker.ready.then((worker) => {
         worker.sync.register('syncdata');
       }))
--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
+const CACHE = 'cache-update-and-refresh-v1';
+
 self.addEventListener('install', (event) => {
     console.log('Установлен');
+
+    event.waitUntil(caches.open(CACHE).then(function (cache) {
+    cache.addAll([
+      '/sw-testing'
+    ]);
+  }));
 });
 
 self.addEventListener('activate', (event) => {
@@ -8,4 +16,40 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     console.log('Происходит запрос на сервер');
+    event.respondWith(fromCache(event.request));
+    event.waitUntil(
+      update(event.request)
+      // В конце, после получения "свежих" данных от сервера уведомляем всех клиентов.
+      .then(refresh)
+    );
 });
+
+function fromCache(request) {
+    return caches.open(CACHE).then((cache) =>
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
+}
+
+function update(request) {
+    return caches.open(CACHE).then((cache) =>
+        fetch(request).then((response) =>
+            cache.put(request, response.clone()).then(() => response)
+        )
+    );
+}
+
+// Шлём сообщения об обновлении данных всем клиентам.
+function refresh(response) {
+    return self.clients.matchAll().then((clients) => {
+        clients.forEach((client) => {
+            const message = {
+                type: 'refresh',
+                url: response.url,
+                eTag: response.headers.get('ETag')
+            };
+            // Уведомляем клиент об обновлении данных.
+            client.postMessage(JSON.stringify(message));
+        });
+    });
+}
