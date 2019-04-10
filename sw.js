ccc3831d565ac31567df3b654a546b6c77cd471b
Dmitry Didenko <ddidenko@smedialink.com>
2019-04-10 09:31:51 +0300

    2

--- sw.js
+++ sw.js
@@ -14,8 +14,8 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-      './index.html',
-      './cached'
+      'index.html',
+      '/cached'
     ]);
   });
 }
