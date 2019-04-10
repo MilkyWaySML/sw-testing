54e6cddc0d04e1ee36ed2eef44d7448d21ed619c
Dmitry Didenko <ddidenko@smedialink.com>
2019-04-10 09:48:43 +0300

    132

--- sw.js
+++ sw.js
@@ -2,7 +2,12 @@ var CACHE = 'cache-and-update';
 
 self.addEventListener('install', function(evt) {
   console.log('The service worker is being installed.');
-  evt.waitUntil(precache());
+  evt.waitUntil(caches.open(CACHE).then(function (cache) {
+    cache.addAll([
+      '/sw-testing/index.html',
+      '/sw-testing/cached'
+    ]);
+  }));
 });
 
 self.addEventListener('fetch', function(evt) {
@@ -10,15 +15,6 @@ self.addEventListener('fetch', function(evt) {
   evt.respondWith(fromCache(evt.request));
   evt.waitUntil(update(evt.request));
 });
-
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll([
-      'cached/',
-      'index.html',
-    ]);
-  });
-}
 
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
