28ca4b5f9f56b7d8c6c70c2adb145bffe39af739
Dmitry Didenko <ddidenko@smedialink.com>
2019-04-10 09:11:29 +0300

    123

--- sw.js
+++ sw.js
@@ -1,54 +1,37 @@
-const CACHE = 'cache-update-and-refresh-v1';
-self.addEventListener('install', (event) => {
-    console.log('Установлен');
+var CACHE = 'cache-and-update';
 
-    event.waitUntil(caches.open(CACHE).then(function (cache) {
-    cache.addAll([
-      'https://milkywaysml.github.io/sw-testing/cached/'
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache());
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntil(update(evt.request));
+});
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      './index.html',
+      './cached'
     ]);
-  }));
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Активирован');
-});
-
-self.addEventListener('fetch', (event) => {
-    console.log('Происходит запрос на сервер');
-    event.respondWith(fromCache(event.request));
-    event.waitUntil(
-      update(event.request)
-      // В конце, после получения "свежих" данных от сервера уведомляем всех клиентов.
-      .then(refresh)
-    );
-});
+  });
+}
 
 function fromCache(request) {
-    return caches.open(CACHE).then((cache) =>
-        cache.match(request).then((matching) =>
-            matching || Promise.reject('no-match')
-        ));
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
 }
 
 function update(request) {
-    return caches.open(CACHE).then((cache) =>
-        fetch(request).then((response) =>
-            cache.put(request, response.clone()).then(() => response)
-        )
-    );
-}
-
-// Шлём сообщения об обновлении данных всем клиентам.
-function refresh(response) {
-    return self.clients.matchAll().then((clients) => {
-        clients.forEach((client) => {
-            const message = {
-                type: 'refresh',
-                url: response.url,
-                eTag: response.headers.get('ETag')
-            };
-            // Уведомляем клиент об обновлении данных.
-            client.postMessage(JSON.stringify(message));
-        });
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
     });
+  });
 }
