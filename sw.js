b1fbd7c0db9d48500adee4d182c7adbff1074599
Dmitry Didenko <ddidenko@smedialink.com>
2019-04-10 14:34:21 +0300

    add sw

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE = 'cache-and-update';
+
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(caches.open(CACHE).then(function (cache) {
+    cache.addAll([
+      '/sw-testing/index.html',
+      '/sw-testing/'
+    ]);
+  }));
+});
+
+self.addEventListener('fetch', function(evt) {
+  const requestURL = new URL(evt.request.url);
+
+  if(!/(api.stocity.ru)/.test(requestURL.pathname)){
+    evt.respondWith(fromCache(evt.request));
+    evt.waitUntil(update(evt.request));
+  } else {
+    console.log(`api request ${evt.request.url}`);
+  }
+});
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
