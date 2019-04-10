587e628eb8971e039dd7085265c9e46eb12b9523
Dmitry Didenko <ddidenko@smedialink.com>
2019-04-10 14:32:21 +0300

    update sw with api test

--- back/sw.js
+++ back/sw.js
@@ -11,9 +11,6 @@ self.addEventListener('install', function(evt) {
 });
 
 self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  console.log(evt.request);
-  console.log(typeof evt.request.uri);
   const requestURL = new URL(evt.request.url);
 
   if(!/(api.stocity.ru)/.test(requestURL.pathname)){
